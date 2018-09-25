2265112b6f49b9c9459d993fb93a2d57767b4402
syunta <kitagawa@timedia.co.jp>
2018-09-26 00:37:22 +0900

    Improve repl

--- repl/repl.go
+++ repl/repl.go
@@ -3,6 +3,7 @@ package repl
 import (
 	"bufio"
 	"fmt"
+	"github.com/syunta/monkey/evaluator"
 	"github.com/syunta/monkey/lexer"
 	"github.com/syunta/monkey/parser"
 	"io"
@@ -31,8 +32,11 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		io.WriteString(out, program.String())
-		io.WriteString(out, "\n")
+		evaluated := evaluator.Eval(program)
+		if evaluated != nil {
+			io.WriteString(out, program.String())
+			io.WriteString(out, "\n")
+		}
 	}
 }
 
