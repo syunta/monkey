8b4831ab8d274388b0ea20c8fe1567461e523038
syunta <kitagawa@timedia.co.jp>
2018-09-30 20:01:28 +0900

    Add Array len

--- evaluator/builtins.go
+++ evaluator/builtins.go
@@ -9,9 +9,12 @@ var builtins = map[string]*object.Builtin{
 				return newError("wrong number of arguments. got=%d, want=1",
 					len(args))
 			}
+
 			switch arg := args[0].(type) {
 			case *object.String:
 				return &object.Integer{Value: int64(len(arg.Value))}
+			case *object.Array:
+				return &object.Integer{Value: int64(len(arg.Elements))}
 			default:
 				return newError("argument to `len` not supported, got %s",
 					args[0].Type())
