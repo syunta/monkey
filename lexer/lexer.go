62ebabac72f1c29be298eb4787ebc7746890c2e4
syunta <kitagawa@timedia.co.jp>
2018-09-30 14:58:03 +0900

    Add bracket token

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -79,6 +79,10 @@ func (l *Lexer) NextToken() token.Token {
 	case '"':
 		tok.Literal = l.readString()
 		tok.Type = token.STRING
+	case '[':
+		tok = newToken(token.LBRACKET, l.ch)
+	case ']':
+		tok = newToken(token.RBRACKET, l.ch)
 	default:
 		if isLetter(l.ch) {
 			tok.Literal = l.readIdentifier()
